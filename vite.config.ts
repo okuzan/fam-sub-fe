697da93fdeb9a320ddb3acfbdaa759a84714361a
okuzan <kuzanoleg@gmail.com>
2026-03-26 10:55:50 +0200

    polling, file watcher

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 9000
-  }
+    plugins: [react()],
+    server: {
+        port: 9000,
+        watch: {
+            usePolling: true,
+            interval: 1000,
+            binaryInterval: 1500,
+            ignored: [
+                '**/node_modules/**',
+                '**/.git/**',
+                '**/dist/**',
+                '**/coverage/**',
+                '**/.idea/**',
+                '**/.vscode/**',
+            ],
+        },
+    },
 })
